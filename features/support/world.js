44356681f629366cdad0998df1fc0f28f6d13de1
amanda_gibson <gibson.amanda.mae@gmail.com>
2019-01-29 19:42:43 +0100

    adds passing tests for no contacts in address book, updates html body

--- features/support/world.js
+++ features/support/world.js
@@ -7,20 +7,27 @@ const HOME_PAGE = 'http://localhost:3000'
 class AddressBookWorld {
   constructor() {}
 
-  // Open the home page using puppeteer
   async openHomePage() {
     this.browser = await puppeteer.launch({headless: false, slowmo: 100})
     this.page = await this.browser.newPage()
     await this.page.goto(HOME_PAGE)
   }
+
   async closeHomePage() {
     await this.browser.close()
   }
+
   async pageHasTextContent(expectedContent) {
     const pageContent = await this.page.content()
     const actualContent = pageContent.match(expectedContent)[0]
 
     expect(actualContent).to.be.eq(expectedContent)
+  }
+
+  async clickOnAddContactBtn() {
+    const btnSelector = '.add-contact'
+    await this.page.waitForSelector(btnSelector)
+    await this.page.click(btnSelector)
   }
 }
 
