4135f393a66942e153492eed070d7396c66a0691
amanda_gibson <gibson.amanda.mae@gmail.com>
2019-02-27 02:42:31 +0100

    delete functionalitly

--- src/app.js
+++ src/app.js
@@ -1,56 +1,75 @@
+const storage = window.localStorage;
 const renderContacts = () => {
-  const storage = window.localStorage
   // Read all the contacts from the storage
-  const contacts = JSON.parse(storage.getItem('contacts'))
+  const contacts = JSON.parse(storage.getItem("contacts"));
 
   // Select the container we will use to list the contacts
-  let div = document.querySelector('.contact-list')
+  let div = document.querySelector(".contact-list");
 
   if (contacts) {
-    div.innerHTML = ''
+    div.innerHTML = "";
 
     // render the contacts
-    const ul = document.createElement('ul')
+    const ul = document.createElement("ul");
 
     // For every contact in our array of contacts, we will
     // create a li element that will contain a card with
     // all the information of the contact
     contacts.forEach(contact => {
-      let li = document.createElement('li')
-      li.setAttribute("class", "list-reset")
+      let li = document.createElement("li");
+      li.id = contact.id;
+      li.setAttribute("class", "list-reset");
       li.innerHTML = `
       <div class="card mb-4" >
       </div>
       <div class="content list-reset">
       <br>
-      <h3>${ contact.name }</h3>
-      <h5>${ contact.company }</h5>
-      <p>${ contact.notes }</p>
-      ${ contact.email } <br>
-      <a href="https://www.twitter.com/${ contact.twitter}">@${contact.twitter}</a>
+      <h3>${contact.name}</h3>
+      <h5>${contact.company}</h5>
+      <p>${contact.notes}</p>
+      ${contact.email} <br>
+      <a href="https://www.twitter.com/${contact.twitter}">@${
+        contact.twitter
+      }</a>
       </div>
       </div><br>
-      const deleteButton = document.createElement('button')
-      // Add the contact's li to the unordered list we created earlier
-      ul.appendChild(li)
-    })
+      `;
+      const deleteButton = document.createElement("button");
+      deleteButton.classList = "delete";
+      deleteButton.innerHTML = "Delete";
+      li.appendChild(deleteButton);
+      ul.appendChild(li);
+    });
 
     // Lastly, append the list to the contact-list container.
-    div.appendChild(ul)
+    div.appendChild(ul);
   } else {
-    div.innerHTML = '<p>You have no contacts in your address book</p>'
+    div.innerHTML = "<p>You have no contacts in your address book</p>";
   }
-}
+};
 
+document.addEventListener("DOMContentLoaded", () => {
+  renderContacts();
 
-document.addEventListener('DOMContentLoaded', () => {
-  renderContacts()
+  const addContactForm = document.querySelector(".new-contact-form");
+  const deleteButton = document.querySelector(".contact-list");
 
-  const addContactForm = document.querySelector('.new-contact-form')
+  deleteButton.addEventListener("click", event => {
+    let id = event.target.parentNode.id;
+    let contacts = JSON.parse(storage.getItem("contacts")) || [];
 
-  addContactForm.addEventListener('submit', event => {
-    event.preventDefault()
-    const storage = window.localStorage
+    contacts.forEach(contact => {
+      if (contact.id == id) {
+        contacts.splice(contacts.indexOf(contact), 1);
+      } else return;
+    });
+
+    storage.setItem("contacts", JSON.stringify(contacts));
+    renderContacts();
+  });
+  addContactForm.addEventListener("submit", event => {
+    event.preventDefault();
+    const storage = window.localStorage;
 
     const {
       name,
@@ -58,8 +77,8 @@ document.addEventListener('DOMContentLoaded', () => {
       phone,
       company,
       notes,
-      twitter,
-    } = addContactForm.elements
+      twitter
+    } = addContactForm.elements;
 
     const contact = {
       id: Date.now(),
@@ -68,13 +87,13 @@ document.addEventListener('DOMContentLoaded', () => {
       phone: phone.value,
       company: company.value,
       notes: notes.value,
-      twitter: twitter.value,
-    }
+      twitter: twitter.value
+    };
 
-    let contacts = JSON.parse(storage.getItem('contacts')) || []
-    contacts.push(contact)
-    storage.setItem('contacts', JSON.stringify(contacts))
-    renderContacts()
-    addContactForm.reset()
-  })
-})
+    let contacts = JSON.parse(storage.getItem("contacts")) || [];
+    contacts.push(contact);
+    storage.setItem("contacts", JSON.stringify(contacts));
+    renderContacts();
+    addContactForm.reset();
+  });
+});
